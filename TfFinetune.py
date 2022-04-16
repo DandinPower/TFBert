a33c8d0d74cb80a312564a2061b8063c737e4b0b
DandinPower <t108590045@ntut.org.tw>
2022-04-17 00:47:41 +0800

    init

---
+++ TfFinetune.py
@@ -0,0 +1,60 @@
+from models.bert.configs import Config
+from models.bert.modeling import BERTModel,BERTClassifier
+from models.preprocess.data import YelpDataset,load_vocab,DataLoader,GetTrainDataset,GetTestDataset,GetSingleDataset
+from models.preprocess.load import load_variable,Parameters,LoadModel,SaveModel,WriteTfLite
+from models.train.classification import Train,Inference
+from models.valid.tflite import TfliteTest
+from dotenv import load_dotenv
+import os
+import numpy as np
+import tensorflow as tf
+load_dotenv()
+
+PARAMETER_PATH = os.getenv('PARAMETER_PATH')
+MODEL_SAVE_PATH = os.getenv('MODEL_SAVE_PATH')
+DATASET_PATH = os.getenv('DATASET_PATH')
+MAX_LEN = int(os.getenv('MAX_LEN'))
+SPLIT_RATE = float(os.getenv('SPLIT_RATE'))
+NUM_EPOCHS = int(os.getenv('NUM_EPOCHS'))
+LR = float(os.getenv('LR'))
+BATCH_SIZE = int(os.getenv('BATCH_SIZE'))
+TFLITE_PATH = os.getenv('TFLITE_PATH')
+
+
+def SingleTest():
+    config = Config()
+    datas,labels = GetSingleDataset(DATASET_PATH,MAX_LEN,SPLIT_RATE)
+    singleData = datas[1]
+    singleLabels = labels[1]
+    newModel = LoadModel(MODEL_SAVE_PATH)
+    output = newModel(singleData)
+    print(output)
+    loss = tf.keras.losses.sparse_categorical_crossentropy(y_true=[1,0], y_pred=output)
+    loss = tf.reduce_mean(loss) 
+    print(loss)
+
+def OnlyInference():
+    datas,labels = GetTestDataset(DATASET_PATH,MAX_LEN,SPLIT_RATE,BATCH_SIZE)
+    newModel = LoadModel(MODEL_SAVE_PATH)
+    Inference(newModel,datas, labels)
+
+def main():
+    config = Config()
+    parameters = load_variable(PARAMETER_PATH)
+    parameters = Parameters(parameters)
+    model = BERTClassifier(config, parameters)
+    model.LoadParameters()
+    datas,labels = GetTrainDataset(DATASET_PATH,MAX_LEN,SPLIT_RATE,BATCH_SIZE)
+    model = Train(model,datas, labels, LR, NUM_EPOCHS,MODEL_SAVE_PATH)
+    testDatas,testLabels = GetTestDataset(DATASET_PATH,MAX_LEN,SPLIT_RATE,BATCH_SIZE)
+    Inference(model,datas, labels)
+    SaveModel(model, MODEL_SAVE_PATH)
+    newModel = LoadModel(MODEL_SAVE_PATH)
+    Inference(newModel,datas, labels)
+
+if __name__ == "__main__":
+    #SingleTest()
+    #OnlyInference()
+    #main()
+    #WriteTfLite(MODEL_SAVE_PATH, TFLITE_PATH)
+    TfliteTest()
---
+++ models/valid/tflite.py
@@ -0,0 +1,65 @@
+from ..preprocess.data import GetTestDataset
+import tensorflow as tf 
+from progressbar import ProgressBar
+from dotenv import load_dotenv
+import numpy as np
+load_dotenv()
+import os 
+import time
+
+TFLITE_PATH = os.getenv('TFLITE_PATH')
+DATASET_PATH = os.getenv('DATASET_PATH')
+MAX_LEN = int(os.getenv('MAX_LEN'))
+SPLIT_RATE = float(os.getenv('SPLIT_RATE'))
+BATCH_SIZE = int(os.getenv('BATCH_SIZE'))
+
+def TfliteTest():
+    print("Tflite Inferencing...")
+    interpreter = GetInterpreter()
+    metrics = tf.metrics.SparseCategoricalAccuracy()
+    batchDatas, batchLabels = GetTestDataset(DATASET_PATH,MAX_LEN,SPLIT_RATE,BATCH_SIZE)
+    now = 0
+    total = len(batchDatas)
+    start = time.time()
+    pBar = ProgressBar().start()
+    for data,label in zip(batchDatas,batchLabels):
+        y_pred = TfliteInference(interpreter, data, label)
+        metrics.update_state(label,y_pred)
+        pBar.update(int((now / (total - 1)) * 100))
+        now += 1
+    pBar.finish()
+    print(f'cost time: {round(time.time() - start,3)} sec')
+    print(f'Lite accuracy:{metrics.result().numpy()}')
+
+def TfliteInference(interpreter,datas,labels):
+    inputs = interpreter.get_input_details()[0]['index']
+    outputs = interpreter.get_output_details()[0]['index']
+    interpreter.set_tensor(inputs, datas)
+    interpreter.invoke()
+    return interpreter.get_tensor(outputs)
+    
+def RandomTest(interpreter):
+    input_details = interpreter.get_input_details()
+    output_details = interpreter.get_output_details()
+    input_shape = input_details[0]['shape']
+    input_data = np.array(np.random.random_sample(input_shape), dtype=np.int32)
+    print(input_data)
+    interpreter.set_tensor(input_details[0]['index'], input_data)
+    interpreter.invoke()
+    tflite_model_predictions = interpreter.get_tensor(output_details[0]['index'])
+    print(tflite_model_predictions)
+
+def GetInterpreter():
+    interpreter = tf.lite.Interpreter(model_path=TFLITE_PATH)
+    interpreter.resize_tensor_input(0, [BATCH_SIZE, MAX_LEN])
+    interpreter.allocate_tensors()
+    input_details = interpreter.get_input_details()
+    output_details = interpreter.get_output_details()
+    print("Input Shape:",input_details[0]['shape'])
+    print("Input Type:",input_details[0]['dtype'])
+    print("Output Shape:",output_details[0]['shape'])
+    print("Output Type:",output_details[0]['dtype'])
+    return interpreter
+
+if __name__ == "__main__":
+    pass
